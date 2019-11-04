02a8e20f4a024c19d8f531e957ed51a4085942a0
segarber <sgarber7@jhu.edu>
2019-11-03 20:02:08 -0500

    push files I have

---
+++ WeTask-master/app/src/main/java/com/example/wetask/GroupSettings.java
@@ -0,0 +1,16 @@
+package com.example.wetask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GroupSettings extends AppCompatActivity {
+
+    /*Created so I could set up navigation**/
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_group_settings );
+    }
+}
