4ccd51ab3e1cce841f2b50a70f0f08487048e8fd
zoegoldstein <zrgoldstein@hotmail.com>
2019-11-07 00:22:42 -0500

    Playing around with ViewTask layout'

--- app/src/main/java/com/example/wetask/Login.java
+++ app/src/main/java/com/example/wetask/Login.java
@@ -2,13 +2,17 @@ package com.example.wetask;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
 
 public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
-        setContentView( R.layout.activity_view_task );
+        setContentView( R.layout.login);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
     }
 }
 
