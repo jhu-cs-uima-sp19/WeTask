c558f82fdefad6046fbb5d356d9f6f4c6c54464c
segarber <sgarber7@jhu.edu>
2019-11-07 16:45:56 -0500

    launches to login activity and login button launches main activity

--- app/src/main/java/com/example/wetask/Login.java
+++ app/src/main/java/com/example/wetask/Login.java
@@ -2,8 +2,12 @@ package com.example.wetask;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.widget.Toolbar;
 
 public class Login extends AppCompatActivity {
@@ -15,6 +19,15 @@ public class Login extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Button login = findViewById(R.id.login);
+        login.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
 
