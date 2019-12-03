8a1ee4226de32d5b064418f0087205bddabae4dd
segarber <sgarber7@jhu.edu>
2019-12-03 03:15:47 -0500

    inconsistent height issue in task cards now resolved--they're squares. width-height ratio can be easily adjusted but this seemed like a good compromise

---
+++ app/src/main/java/com/example/wetask/CardLayout.java
@@ -0,0 +1,27 @@
+package com.example.wetask;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+public class CardLayout extends LinearLayout {
+
+    public CardLayout(Context context) {
+        super(context);
+    }
+
+    public CardLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CardLayout(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onMeasure(int width, int height) {
+        // note we are applying the width value as the height
+        super.onMeasure(width, width);
+    }
+
+}
